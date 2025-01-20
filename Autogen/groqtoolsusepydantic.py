c055c1273ca3d9043a7006d5d55feb087652abe1
sundara26071978 <sunjsg@gmail.com>
2025-01-20 06:32:08 +0530

    Add files via upload

---
+++ Autogen/groqtoolsusepydantic.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+
+import os
+from typing import Annotated, Literal
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ["GROQ_API_KEY1"]=os.getenv("GROQ_API_KEY1")
+
+
+config_list = [
+    {
+        # Let's choose the Mixtral 8x22B model
+        "model": "llama3-8b-8192",
+        # Provide your Mistral AI API key here or put it into the MISTRAL_API_KEY environment variable.
+        "api_key": os.environ.get("GROQ_API_KEY1"),
+        # We specify the API Type as 'mistral' so it uses the Mistral AI client class
+        "api_type": "groq",
+    }
+]
+
+
+from pydantic import BaseModel, Field
+
+Operator = Literal["+", "-", "*", "/"]
+class CalculatorInput(BaseModel):
+    a: Annotated[int, Field(description="The first number.")]
+    b: Annotated[int, Field(description="The second number.")]
+    operator: Annotated[Operator, Field(description="The operator.")]
+
+
+def calculator(input: Annotated[CalculatorInput, "Input to the calculator."]) -> int:
+    if input.operator == "+":
+        return input.a + input.b
+    elif input.operator == "-":
+        return input.a - input.b
+    elif input.operator == "*":
+        return input.a * input.b
+    elif input.operator == "/":
+        return int(input.a / input.b)
+    else:
+        raise ValueError("Invalid operator")
+
+
+
+
+import os
+
+from autogen import ConversableAgent
+
+# Let's first define the assistant agent that suggests tool calls.
+assistant = ConversableAgent(
+    name="Assistant",
+    system_message="You are a helpful AI assistant. "
+    "You can help with simple calculations. "
+    "Return 'TERMINATE' when the task is done.",
+    llm_config={"config_list": config_list},
+)
+
+# The user proxy agent is used for interacting with the assistant agent
+# and executes tool calls.
+user_proxy = ConversableAgent(
+    name="User",
+    llm_config=False,
+    is_termination_msg=lambda msg: msg.get("content") is not None and "TERMINATE" in msg["content"],
+    human_input_mode="NEVER",
+)
+
+# # Register the tool signature with the assistant agent.
+# assistant.register_for_llm(name="calculator", description="A simple calculator")(calculator)
+
+# # Register the tool function with the user proxy agent.
+# user_proxy.register_for_execution(name="calculator")(calculator)
+
+
+#or
+
+
+from autogen import register_function
+
+# Register the calculator function to the two agents.
+register_function(
+    calculator,
+    caller=assistant,  # The assistant agent can suggest calls to the calculator.
+    executor=user_proxy,  # The user proxy agent can execute the calculator calls.
+    name="calculator",  # By default, the function name is used as the tool name.
+    description="A simple calculator",  # A description of the tool.
+)
+
+
+print(assistant.llm_config["tools"])
+
+chat_result = user_proxy.initiate_chat(assistant, message="What is (1423 - 123) / 3 + (32 + 23) * 5?")
